5527b88918294276d950b90b65a3984a8eb08165
ALASKA_KOLASKA <thefreshchannel@mail.ru>
2023-01-09 00:06:44 +0300

    add layout and grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from "react";
 import './App.css';
-import Technologies from "./techonologies";
-import Header from "./header";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://fis.ru/popup_imgs/11771127.jpg'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://avatars.dzeninfra.ru/get-zen_doc/4488022/pub_61a71b2193bf1922fb288704_61a71bce93bcb15eb8977512/scale_1200'/>
+                </div>
+                <div>
+                    ava+description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
