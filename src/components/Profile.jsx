e498b20588e24f27e55de56c7c64504e9de163c0
ALASKA_KOLASKA <thefreshchannel@mail.ru>
2023-01-14 02:23:04 +0300

    add css-modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
             <img
                 src='https://avatars.dzeninfra.ru/get-zen_doc/4488022/pub_61a71b2193bf1922fb288704_61a71bce93bcb15eb8977512/scale_1200'/>
@@ -14,11 +15,11 @@ const Profile = () => {
             <div>
                 New post
             </div>
-            <div>
-                <div>
+            <div className={s.posts}>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
