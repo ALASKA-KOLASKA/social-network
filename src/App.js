1b3476da71827999e07ad7c5ce3d808234189838
ALASKA_KOLASKA <thefreshchannel@mail.ru>
2023-01-27 00:07:12 +0300

    add UI-BLL separation for dialogs, messages and posts from App.js

--- src/App.js
+++ src/App.js
@@ -9,29 +9,7 @@ import News from "./components/Content/News/News.jsx";
 import Music from "./components/Content/Music/Music.jsx";
 import Settings from "./components/Content/Settings/Settings.jsx";
 
-const App = () => {
-
-    let dialogs = [
-        {id: 1, name: 'Dima'},
-        {id: 2, name: 'Andrew'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Victor'},
-        {id: 6, name: 'Valera'}
-    ]
-
-    let messages = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'}
-    ]
-
-    let posts = [
-        {id: 1, message: 'Hi, how are you?', likesCount: '0'},
-        {id: 2, message: 'It\'s my first post', likesCount: '23'}
-    ]
+const App = (props) => {
 
     return (
         <BrowserRouter>
@@ -40,8 +18,8 @@ const App = () => {
                 <Navbar/>
                 <div className={s.library_background}>
                     <Routes>
-                        <Route path='/dialogs' element={<Dialogs dialogs={dialogs} messages={messages}/>}/>
-                        <Route path='/profile' element={<Profile posts={posts}/>}/>
+                        <Route path='/dialogs' element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
+                        <Route path='/profile' element={<Profile posts={props.posts}/>}/>
                         <Route path='/news' element={<News/>}/>
                         <Route path='/music' element={<Music/>}/>
                         <Route path='/settings' element={<Settings/>}/>
